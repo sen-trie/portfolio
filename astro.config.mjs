59c8ab5e3fd0941406e0f61692a5a25f2c247374
ayaya-pc <122269172+sen-trie@users.noreply.github.com>
2025-09-09 20:06:30 +0800

    Switch all <Images> to <Pictures>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sen-trie.github.io/portfolio',
-  base: 'portfolio',
+  site: "https://sen-trie.github.io/portfolio",
+  base: "portfolio",
   publicDir: "static",
   integrations: [
     purgecss({
-      keyframes: false, 
+      keyframes: false,
       safelist: {
-        greedy: [/*astro*/]
-      }
-    })
-  ]
+        greedy: [
+          /*astro*/
+        ],
+      },
+    }),
+  ],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  image: {
+    responsiveStyles: true,
+  },
 });
